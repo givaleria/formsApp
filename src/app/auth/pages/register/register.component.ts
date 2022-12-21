27fb24a1e8bcd9d2a9d921b5cc090b92a8fa5b5d
gisela degiglio <giselavaleria19@gmail.com>
2022-12-21 14:01:13 -0300

    end of section 18

--- src/app/auth/pages/register/register.component.ts
+++ src/app/auth/pages/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { EmailValidatorService } from 'src/app/shared/validator/email-validator.service';
 import { ValidatorService } from '../../../shared/validator/validator.service';
 
 @Component({
@@ -12,7 +13,7 @@ export class RegisterComponent implements OnInit {
 
   myForm: FormGroup = this.fb.group({
     name: ['', [Validators.required, Validators.pattern(this.validatorService.fullNamePattern)] ],
-    email: ['', [Validators.required, Validators.pattern(this.validatorService.emailPattern)] ],
+    email: ['', [Validators.required, Validators.pattern(this.validatorService.emailPattern)], [this.emailValidator] ],
     username: ['', [Validators.required, this.validatorService.cantBeStrider] ],
     password: ['', [Validators.required, Validators.minLength(6)] ],
     password2: ['', [Validators.required] ],
@@ -22,16 +23,35 @@ export class RegisterComponent implements OnInit {
     ]
   })
 
+  get emailErrorMsg(): string {
+
+    const errors = this.myForm.get('email')?.errors;
+
+    if (errors?.['required']) {
+      return 'Email is mandatory.'
+    } else if (errors?.['pattern']) {
+      return 'Invalid email format.'
+    } else if (errors?.['takenEmail']) {
+      return 'Email has already been taken.'
+    }
+
+    return '';
+
+  }
+
   constructor(
     private fb: FormBuilder,
-    private validatorService: ValidatorService
+    private validatorService: ValidatorService,
+    private emailValidator: EmailValidatorService
   ) { }
 
   ngOnInit(): void {
     this.myForm.reset({
       name: 'Gi Degiglio',
-      email: 'test@huenei.com',
-      username: 'angelofdeath'
+      email: 'test1@test.com',
+      username: 'angelofdeath',
+      password: '123456',
+      password2: '123456'
     })
   }
 
